9717bfdabfc93855781b3565ccf1943f54e87102
Lol89ai <NazarovaElena777@mail.ru>
2026-05-30 22:20:23 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -10,15 +10,12 @@ const CONFIRMATION = "ac333ea1";
 app.post("/callback", (req, res) => {
     console.log("VK EVENT:", req.body);
 
-    const event = req.body;
-
-    if (event.type === "confirmation") {
+    if (req.body.type === "confirmation") {
         return res.send("ac333ea1");
     }
 
     return res.send("ok");
 });
-
 app.get("/", (req, res) => {
     res.send("VK bridge is running");
 });
